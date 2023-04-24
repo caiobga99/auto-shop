d4ecefeb365037f87fb835ceddfaf51c1ef54cb1
caiobga99 <caiobga99@gmail.com>
2023-04-23 22:17:25 -0300

    web - translate.

--- web/src/analyticsEventTracker.js
+++ web/src/analyticsEventTracker.js
@@ -1,7 +1,7 @@
 import ReactGA from "react-ga";
 
-const analyticsEventTracker = (category = "Blog category") => {
-  const eventTracker = (action = "test action", label = "test label") => {
+const analyticsEventTracker = (category = "Categoria Site") => {
+  const eventTracker = (action = "Ação teste", label = "Teste label") => {
     ReactGA.event({ category, action, label });
   };
   return eventTracker;
