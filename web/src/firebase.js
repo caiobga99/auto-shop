d87231ff84504fb56cf17768c4a46d5ae0b3cc40
caiobga99 <caiobga99@gmail.com>
2023-05-17 07:33:08 -0300

    web - fixing firebase information.

--- web/src/firebase.js
+++ web/src/firebase.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC4N0mD-wMsv4t6MVWAv1g5cg_OiH_81dk",
@@ -12,5 +15,7 @@ const firebaseConfig = {
   measurementId: "G-T5MNKRMK1W",
 };
 
-export const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase.analytics().logEvent("screen_view");
 export const auth = getAuth();
